cfc2ca5bb931dbcfdbda6bdb73a0a6f6a8b2a7a9
Adrian Perez <Adriancostaspain@gmail.com>
2025-11-07 15:30:56 +0100

    Css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
- // Aquí tu código
+/***Descripción:**
+Este ejercicio consiste en crear una página web donde los usuarios puedan agregar elementos a una lista utilizando un cuadro de diálogo `prompt`. Luego, los elementos ingresados se mostrarán en una lista en la página.
+
+Al pulsar el botón 'agregar elemento' aparecerá el cuadro de dialogo (`prompt`) y ahí se introducirá el elemento que después aparecerá en la lista.
+
+La estructura ya está creada en el HTML, así solo tendrás que trabajar con JavaScript.*/
+
+
+
+const lista = document.getElementById("lista");
+const boton = document.getElementById("agregar");
+
+agregar.addEventListener("click", function(){
+    const producto = prompt("Escribe aqui tu producto")
+    const nuevoelemento = document.createElement("li")
+    nuevoelemento.textContent = producto;
+    lista.appendChild(nuevoelemento);
+})
